dde47ec5390828070a521600c1194890d40edce5
kouchpouch <nakouchdoge@gmail.com>
2026-01-07 22:57:30 -0600

    moved to src directory

--- helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-extern int upper(char* ltr); // Returns ascii value to uppercase
-extern int lower(char* ltr); // Returns ascii value to lowercase
-// Checks to see if a day exists in a month, returns maximum days in that month
-extern bool dayexists(int d, int m, int y);
-
-#endif
--- sorter.h
+++
@@ -1,8 +0,0 @@
-#ifndef SORTER_H
-#define SORTER_H
-
-/* Returns a the line in the CSV to insert the data sorted by date
- * , assumes the CSV is already sorted by date */
-extern int sort_csv(int month, int day, int year, int maxlines); 
-
-#endif
