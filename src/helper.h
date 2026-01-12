1d8a2d059c9f9269c2a86805a31c8e72c3e9ddf5
kouchpouch <nakouchdoge@gmail.com>
2026-01-12 11:42:46 -0600

    Made comments consistent with other header files

--- src/helper.h
+++ src/helper.h
@@ -1,9 +1,14 @@
 #ifndef HELPER_H
 #define HELPER_H
 
-extern int upper(char* ltr); // Returns ascii value to uppercase
-extern int lower(char* ltr); // Returns ascii value to lowercase
-// Checks to see if a day exists in a month, returns maximum days in that month
+/* Returns ltr ascii value to uppercase */
+extern int upper(char* ltr);
+
+/* Returns ltr ascii value to lowercase */
+extern int lower(char* ltr);
+
+/* Checks to see if a day exists in a month, 
+ * returns maximum days in that month */
 extern bool dayexists(int d, int m, int y);
 
 #endif
