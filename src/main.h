7ac07a1563404486f67000d780e40d613124c2d2
kouchpouch <nakouchdoge@gmail.com>
2026-01-24 17:56:17 -0600

    Init

---
+++ src/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define CSV_DIR "./data.csv"
+#define CSV_BAK_DIR "./data.csv.bak"
+#define CSV_FIELDS 7
+#define TEMP_FILE_DIR "./tmp.txt"
+#define LINE_BUFFER 200
+#define STDIN_LARGE_BUFF 64
+#define STDIN_SMALL_BUFF 8
+#define REALLOC_FLAG 64
+
+#endif
