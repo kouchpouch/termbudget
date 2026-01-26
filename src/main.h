4c2dd997b52715c5cd7834abaaa0b10190fb3975
kouchpouch <nakouchdoge@gmail.com>
2026-01-26 14:09:32 -0600

    Renamed macros to explicitly define directories

--- src/main.h
+++ src/main.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define CSV_DIR "./data.csv"
-#define CSV_BAK_DIR "./data.csv.bak"
-#define TEMP_FILE_DIR "./tmp.csv"
+#define RECORD_DIR "./data.csv"
+#define RECORD_BAK_DIR "./data.csv.bak"
 #define CSV_FIELDS 7
+#define TEMP_FILE_DIR "./tmp"
+#define BUDGET_DIR "./budget.csv"
+#define BUDGET_BAK_DIR "./budget.csv.bak"
 #define LINE_BUFFER 200
 #define STDIN_LARGE_BUFF 64
 #define STDIN_SMALL_BUFF 8
 #define REALLOC_FLAG 64
+
+#define MIN_YEAR 1000
+#define MAX_YEAR 9999
 
 #endif
