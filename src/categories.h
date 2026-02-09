7e3c827c9b11fa07a9969ad1f604fc49d7de3a79
kouchpouch <nakouchdoge@gmail.com>
2026-02-08 22:06:00 -0600

    Added comments

--- src/categories.h
+++ src/categories.h
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
 #ifndef CATEGORIES_H
 #define CATEGORIES_H
 
@@ -19,12 +33,19 @@ struct categoryroot {
 	long byteoffset;
 };
 
+/*
+ * CategoryNode is to be used as a doubly linked list which includes members
+ * of CategoryNode pointer next and prev, long catg_fp stores the byte offset
+ * of the category in BUDGET_DIR (from main.h), and a Vec containing all
+ * records in RECORD_DIR (from main.h) that match the category field at catg_fp
+ * in BUDGET_DIR.
+ */
 typedef struct CategoryNode CategoryNode;
 
 struct CategoryNode {
 	CategoryNode *next;
 	CategoryNode *prev;
-	long byteoffset;
+	long catg_fp; // Category File Position
 	Vec *data;
 };
 
