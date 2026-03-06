bfa193de92927bc35b13dc64d6a9476d23c7ca2a
kouchpouch <nakouchdoge@gmail.com>
2026-03-05 21:00:38 -0600

    init

---
+++ src/get_date.c
@@ -0,0 +1,30 @@
+/*
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+#include <time.h>
+
+int get_current_month(void) {
+	time_t timer;
+	time(&timer);
+	struct tm *tm = localtime(&timer);
+	return tm->tm_mon;
+}
+
+int get_current_year(void) {
+	time_t timer;
+	time(&timer);
+	struct tm *tm = localtime(&timer);
+	return tm->tm_year + 1900;
+}
---
+++ src/get_date.h
@@ -0,0 +1,25 @@
+/*
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+#ifndef GET_DATE_H
+#define GET_DATE_H
+
+/* Returns the current month [0-11] */
+extern int get_current_month(void);
+
+/* Returns the current year */
+extern int get_current_year(void);
+
+#endif
