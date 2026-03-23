a2aed150302a79ddfd3112c8f3a14fffc08f6226
kouchpouch <nakouchdoge@gmail.com>
2026-03-23 16:34:06 -0500

    Formatting

--- src/parser.h
+++ src/parser.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License along 
  * with this program. If not, see <https://www.gnu.org/licenses/>. 
  */
+
 #ifndef PARSER_H
 #define PARSER_H
 #include <stdio.h>
