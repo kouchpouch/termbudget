5c3acf5f625966167815d1861115c26f26b85381
kouchpouch <nakouchdoge@gmail.com>
2026-03-22 21:29:44 -0500

    Commented out function which is defined in main.c

--- src/categories.c
+++ src/categories.c
@@ -3,17 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double get_expenditures_per_category(struct BudgetTokens *bt) {
-	double total = 0;
-	Vec *pi = get_records_by_any(bt->m, -1, bt->y, bt->catg, NULL, TT_INCOME, -1, NULL);
-	Vec *pe = get_records_by_any(bt->m, -1, bt->y, bt->catg, NULL, TT_EXPENSE, -1, NULL);
-	for (size_t i = 0; i < pi->size; i++) {
-		total += get_amount(pi->data[i]);
-	}
-	for (size_t i = 0; i < pe->size; i++) {
-		total -= get_amount(pe->data[i]);
-	}
-	free(pe);
-	free(pi);
-	return total;
-}
+//double get_expenditures_per_category(struct BudgetTokens *bt) {
+//	double total = 0;
+//	Vec *pi = get_records_by_any(bt->m, -1, bt->y, bt->catg, NULL, TT_INCOME, -1, NULL);
+//	Vec *pe = get_records_by_any(bt->m, -1, bt->y, bt->catg, NULL, TT_EXPENSE, -1, NULL);
+//	for (size_t i = 0; i < pi->size; i++) {
+//		total += get_amount(pi->data[i]);
+//	}
+//	for (size_t i = 0; i < pe->size; i++) {
+//		total -= get_amount(pe->data[i]);
+//	}
+//	free(pe);
+//	free(pi);
+//	return total;
+//}
