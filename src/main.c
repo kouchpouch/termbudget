af525ccbdc2bd4b1ec7f25eb2e53cd26ac57a10e
kouchpouch <nakouchdoge@gmail.com>
2026-03-24 21:39:13 -0500

    Removed category editing functions (moved to edit_categories.c), removed temporary file management (moved to filemanagement.c)

--- src/main.c
+++ src/main.c
@@ -22,7 +22,9 @@
 #include <ctype.h>
 #include <ncurses.h>
 #include <limits.h>
+
 #include "main.h"
+#include "edit_categories.h"
 #include "get_date.h"
 #include "fileintegrity.h"
 #include "filemanagement.h"
@@ -44,13 +46,6 @@ enum SortBy {
 	SORT_DATE = 0,
 	SORT_CATG = 1
 } sortby;
-
-enum fields {
-	EDIT_AMNT = 0,
-	EDIT_TYPE,
-	ZERO_AMNT,
-	DEL_CATG
-};
 
 const char *abbr_months[] = {
 	"JAN", 
@@ -456,67 +451,6 @@ void print_record_hr(struct LineData *ld) {
 	 );
 }
 
-int nc_select_category_field_to_edit(void) {
-	size_t n_options = 4;
-	struct MenuParams *mp = malloc(sizeof(*mp) + (sizeof(char *) * n_options));
-
-	if (mp == NULL) {
-		memory_allocate_fail();
-	}
-
-	mp->items = n_options;
-	mp->title = "Editing Category";
-	mp->strings[EDIT_AMNT] = "Edit Amount";
-	mp->strings[EDIT_TYPE] = "Edit Type";
-	mp->strings[ZERO_AMNT] = "Zero Out";
-	mp->strings[DEL_CATG] = "Delete";
-
-	int retval = nc_input_menu(mp);
-
-	free(mp);
-	return retval;
-}
-
-bool nc_confirm_amount(double amt) {
-	WINDOW *wptr = create_input_subwindow();
-	mvwxcprintw(wptr, 0, "Confirm Amount");
-	mvwxcprintw(wptr, 3, "Is this amount correct?");
-	mvwprintw(wptr, 4, (getmaxx(wptr) / 2) - (finlen(amt) / 2), "$%.2f", amt);
-	mvwxcprintw(wptr, getmaxy(wptr) - BOX_OFFSET, "(Y)es  /  (N)o");
-
-	int c = 0;
-
-	while (c != KEY_F(QUIT)) {
-		c = wgetch(wptr);
-		switch(c) {
-		case('y'):
-		case('Y'):
-			nc_exit_window(wptr);
-			return true;
-		case('n'):
-		case('N'):
-			nc_exit_window(wptr);
-			return false;
-		default:
-			break;
-		}
-	}
-
-	nc_exit_window(wptr);
-	return false;
-}
-
-/*
- * If the user tries to delete a category that contains members, this warns
- * the user that the action cannot be completed.
- */
-void nc_print_category_member_warning(void) {
-	WINDOW *wptr = create_input_subwindow();
-	mvwxcprintw(wptr, 3, "Cannot delete a category");
-	mvwxcprintw(wptr, 4, "which contains records");
-	nc_exit_window_key(wptr);
-}
-
 void replace_category(struct BudgetTokens *bt, long b) {
 	FILE *bfptr = open_budget_csv("r");
 	FILE *tmpfptr = open_temp_csv();
@@ -544,136 +478,6 @@ void replace_category(struct BudgetTokens *bt, long b) {
 	}
 
 	mv_tmp_to_budget_file(tmpfptr, bfptr);
-}
-
-void delete_category(long b) {
-	FILE *bfptr = open_budget_csv("r");
-	FILE *tmpfptr = open_temp_csv();
-	char *str;
-	char linebuff[LINE_BUFFER];
-	size_t line = boff_to_linenum_budget(b) + 2;
-	size_t linenum = 0;
-
-	while (1) {
-		str = fgets(linebuff, sizeof(linebuff), bfptr);
-		if (str == NULL) {
-			break;
-		}
-		linenum++;	
-		if (linenum != line) {
-			fputs(str, tmpfptr);
-		}
-	}
-
-	mv_tmp_to_budget_file(tmpfptr, bfptr);
-}
-
-void insert_category(struct BudgetTokens *bt, unsigned int ln) {
-	FILE *bfptr = open_budget_csv("r");
-	FILE *tmpfptr = open_temp_csv();
-	
-	char linebuff[LINE_BUFFER];
-	char *line;
-	unsigned int linenum = 0;
-
-	while ((line = fgets(linebuff, sizeof(linebuff), bfptr)) != NULL) {
-		linenum++;	
-		if (linenum != ln) {
-			fputs(line, tmpfptr);
-		} else if (linenum == ln) {
-			fputs(line, tmpfptr);
-			fprintf(tmpfptr, "%d,%d,%s,%d,%.2f\n", 
-				bt->m,
-				bt->y,
-				bt->catg,
-				bt->transtype,
-				bt->amount
-		   );
-		}
-	}
-
-	mv_tmp_to_budget_file(tmpfptr, bfptr);
-
-}
-
-void mv_category_to_top(CategoryNode **nodes, size_t i) {
-	long first = nodes[0]->catg_fp;
-	unsigned int insert_ln = boff_to_linenum_budget(first) + 1;
-
-	struct BudgetTokens *bt = tokenize_budget_byte_offset(nodes[i]->catg_fp);
-
-	delete_category(nodes[i]->catg_fp);
-	insert_category(bt, insert_ln);
-
-	free_budget_tokens(bt);
-}
-
-/* Allows the user to edit the category at file position b. */
-void nc_edit_category(long b, long nmembers) {
-	double tmp = 0.0;
-	enum fields select = nc_select_category_field_to_edit();
-	if (select < 0) {
-		return;
-	}
-	struct BudgetTokens *bt = tokenize_budget_byte_offset(b);
-	if (bt == NULL) {
-		return;
-	}
-
-	switch (select) {
-	case EDIT_AMNT:
-		bt->amount = nc_input_budget_amount();
-		if (bt->amount < 0.0) {
-			free_budget_tokens(bt);
-			return;
-		}
-		bool conf = nc_confirm_amount(bt->amount);
-		if (!conf) {
-			free_budget_tokens(bt);
-			return;
-		}
-		break;
-	case EDIT_TYPE:
-		bt->transtype = nc_input_category_type();
-		if (bt->transtype < 0.0) {
-			free_budget_tokens(bt);
-			return;
-		}
-		break;
-	case ZERO_AMNT:
-		tmp = get_expenditures_per_category(bt);
-		if (bt->transtype == TT_INCOME) {
-			if (tmp < 0.0) {
-				tmp = 0.0;
-			}
-			bt->amount = tmp;
-		} else {
-			if (-(tmp) < 0.0) {
-				tmp = -0.0;
-			}
-			bt->amount = -(tmp);
-		}
-		break;
-	case DEL_CATG:
-		if (nmembers > 0) {
-			nc_print_category_member_warning();
-			free_budget_tokens(bt);
-			return;
-		}
-		if (!nc_confirm_input("Confirm Delete")) {
-			free_budget_tokens(bt);
-			return;
-		}
-		break;
-	}
-
-	if (select != DEL_CATG) {
-		replace_category(bt, b);
-	} else {
-		delete_category(b);
-	}
-
-	free_budget_tokens(bt);
 }
 
 /*
@@ -3000,7 +2804,7 @@ void nc_read_budget_loop(struct ReadWins *wins, FILE *rfptr, FILE *bfptr,
 			}
 			break;
 		case(KEY_SHOME):
-			if (sc->catg_data == -1) {
+			if (sc->catg_data == -1 && sc->catg_node != 0) {
 				mv_category_to_top(nodes, sc->catg_node);
 				sr->flag = RESIZE;
 				sr->index = 0;
@@ -3682,40 +3486,6 @@ err_select_date_fail:
 		break;
 	}
 	refresh();
-}
-
-int move_tmp_to_main(FILE *tmp, FILE *main, char *dir, char *backdir) {
-	if (fclose(main) == -1) {
-		perror("Failed to close main file");
-		return -1;
-	} else {
-		main = NULL;
-	}
-	if (fclose(tmp) == -1) {
-		perror("Failed to close temporary file");
-		return -1;
-	} else {
-		tmp = NULL;
-	}
-	if (rename(dir, backdir) == -1) {
-		perror("Failed to move main file");	
-		return -1;
-	}
-	if (rename(TEMP_FILE_DIR, dir) == -1) {
-		perror("Failed to move temporary file");	
-		return -1;
-	}
-	return 0;
-}
-
-int mv_tmp_to_budget_file(FILE *tmp, FILE* main) {
-	int retval = move_tmp_to_main(tmp, main, BUDGET_DIR, BUDGET_BAK_DIR);
-	return retval;
-}
-
-int mv_tmp_to_record_file(FILE *tmp, FILE* main) {
-	int retval = move_tmp_to_main(tmp, main, RECORD_DIR, RECORD_BAK_DIR);
-	return retval;
 }
 
 int delete_csv_record(int linetodelete) {
