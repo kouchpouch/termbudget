175c79e28a1be4b9b69863e046aaf66582215583
kouchpouch <nakouchdoge@gmail.com>
2026-04-04 19:21:38 -0500

    Init

---
+++ src/read_loops.c
@@ -0,0 +1,36 @@
+/*
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ncurses.h>
+
+#include "read_loops.h"
+#include "read_init.h"
+#include "main.h"
+#include "edit_categories.h"
+#include "get_date.h"
+#include "filemanagement.h"
+#include "helper.h"
+#include "tui.h"
+#include "tui_input.h"
+#include "tui_input_menu.h"
+#include "tui_sidebar.h"
+#include "vector.h"
+#include "parser.h"
+#include "categories.h"
+#include "flags.h"
