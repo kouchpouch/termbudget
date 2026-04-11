1579096793c0a5e8fd887ff807b4c1c28c1389dc
kouchpouch <nakouchdoge@gmail.com>
2026-04-11 13:27:36 -0500

    removed extern keyword

--- src/create.h
+++ src/create.h
@@ -28,24 +28,17 @@ enum AddMainMenu {
 	CREATE_BUDGET
 }; 
 
-extern void insert_budget_record(char *catg, int m, int y, int transtype, double amt);
-
-extern char *create_budget_record(int yr, int mo);
-
-extern void insert_transaction_record(int linetoadd, struct LineData *ld);
+void insert_budget_record(char *catg, int m, int y, int transtype, double amt);
+char *create_budget_record(int yr, int mo);
+void insert_transaction_record(int linetoadd, struct LineData *ld);
 
 /* Optional parameters int month, year. If add transaction is selected while
  * on the read screen these will be auto-filled. */
-extern void create_transaction(int year, int month);
-
-extern void create_transaction_default(void);
-
-extern struct Datevals *nc_create_new_budget(void);
-
-extern int get_add_selection(void);
-
-extern void add_main_with_date(struct Datevals *dv);
-
-extern void add_main_no_date(void);
+void create_transaction(int year, int month);
+void create_transaction_default(void);
+struct Datevals *nc_create_new_budget(void);
+int get_add_selection(void);
+void add_main_with_date(struct Datevals *dv);
+void add_main_no_date(void);
 
 #endif
