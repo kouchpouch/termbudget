89abacb349df8a9882304ee016c610036cba3000
kouchpouch <nakouchdoge@gmail.com>
2026-05-05 14:58:19 -0500

    Removed old function definitions, added new ones

--- src/categories.h
+++ src/categories.h
@@ -24,46 +24,9 @@
 
 #include "vector.h"
 
-/*
- * struct catg_nodes is a doubly linked list which includes members
- * of struct catg_nodes pointer next and prev, long catg_fp stores the byte offset
- * of the category in BUDGET_DIR, and a struct vec_d containing all
- * records in RECORD_DIR that match the category field at catg_fp
- * in BUDGET_DIR.
-
-         |----------|
-         |   Head   | // struct catg_nodes **
-         |----------|
-              |
-              V
-         |------------|      |------------|      |------------|
-         |    next    | ---> |    next    | ---> |    next    | ---> NULL
-         |  1st Node  |      |  2nd Node  | .... |  nth Node  |
-NULL <---|    prev    | <--- |    prev    | <--- |    prev    |
-         |------------|      |------------|      |------------|
-           |       |
-           |       |_________
-           V                |
-        catg_fp             |
-                            V
-                      |----------|
-                      |   size   |
-                      | capacity |
-                      |  []data  | // Each record's FPI that belongs to the
-                      |----------| // node's category.
-*/
-
 struct catg_node { 
 	struct catg_node *next;
 	struct catg_node *prev;
-	struct vec_d *data;
-	long catg_fp;
-};
-
-/* To be replaced */
-struct catg_nodes { 
-	struct catg_nodes *next;
-	struct catg_nodes *prev;
 	struct vec_d *data;
 	long catg_fp;
 };
@@ -75,11 +38,6 @@ struct catg_vec {
 };
 
 void free_categories(struct catg_vec *pc);
-void free_category_nodes(struct catg_nodes **nodes);
-void debug_category_nodes(struct catg_nodes **nodes);
-struct catg_nodes **create_category_nodes(int m, int y);
-
-/* New stuff */
 
 /*
  struct catg_node is a node to be placed inside of a doubly linked list.
@@ -106,6 +64,12 @@ struct catg_nodes **create_category_nodes(int m, int y);
                         |----------| // node's category.
 */
 
+/* Returns a pointer to the node at index "idx" */
+struct catg_node *get_node_by_idx(struct catg_node *head, size_t idx);
+
+/* Returns number of nodes in the linked list */
+int get_total_nodes(struct catg_node *head);
+
 /* Appends a struct catg_node to the tail of the doubly linked list. */
 struct catg_node *append_catg_node(struct catg_node *head,
 								   long catg_fp,
@@ -119,12 +83,18 @@ struct catg_node *insert_catg_node(struct catg_node **head,
 
 /* Deletes node at index "idx" of the doubly linked list. */
 void delete_catg_node(struct catg_node *head, size_t idx);
+
 /* Frees the entire doubly linked list */
 void free_catg_nodes(struct catg_node *head);
+
+/* Moves the node and data of node at "idx" to the head of the linked list */
+void mv_catg_node_to_head(struct catg_node **head, size_t idx);
+
 /* Prints debug data of the entire doubly linked list */
 void debug_print_catg_node_data(struct catg_node *head);
+
 /* Creates all nodes and initializes the members of each node from data found 
- * in BUDGET_DIR and RECORD_DIR */
+ * in BUDGET_DIR and RECORD_DIR, returns the HEAD of the list */
 struct catg_node *create_catg_node_list(int m, int y);
 
 #endif
