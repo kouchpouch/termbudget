77b5894c601a9d1c15120810399cf53c3eee0b2f
kouchpouch <nakouchdoge@gmail.com>
2026-05-05 10:24:11 -0500

    Added a new doubly linked list interface

--- src/categories.c
+++ src/categories.c
@@ -48,16 +48,42 @@ void free_category_nodes(struct catg_nodes **nodes)
 	free(nodes);
 }
 
+static void print_debug_node(struct catg_nodes *node)
+{
+	printw("Data sz: %lu, Next: %p, Prev: %p, FPI: %lu\n", 
+		 node->data->size, 
+		 (void *)node->next, 
+		 (void *)node->prev, 
+		 node->catg_fp);
+}
+
+void debug_category_nodes(struct catg_nodes **nodes)
+{
+	size_t i = 0;
+
+	while (1) {
+		if (nodes[i]->next == NULL) {
+			print_debug_node(nodes[i]);
+			break;
+		} else {
+			print_debug_node(nodes[i]);
+		}
+		i++;
+	}
+}
+
 /*
  * Initializes struct catg_nodes.data. The data is a struct vec_d which contains all of
  * the file position byte offsets for the records that match the struct catg_nodes's
  * category.
  */
-static void init_category_nodes
+void init_category_nodes
 (struct catg_nodes *node, struct vec_d *chunk, int m, int y)
 {
 	struct budget_tokens *budget_tokens = tokenize_budget_fpi(node->catg_fp);
-	struct vec_d *recs = get_records_by_any(m, -1, y, budget_tokens->catg, NULL, -1, -1, chunk);
+	struct vec_d *recs = get_records_by_any(m, -1, y, 
+										    budget_tokens->catg, 
+										    NULL, -1, -1, chunk);
 	node->data = recs;
 	free_budget_tokens(budget_tokens);
 }
@@ -102,4 +128,187 @@ struct catg_nodes **create_category_nodes(int m, int y) {
 	free(chunk);
 	free(catgs_file_pos);
 	return pnode;
+}
+
+/* New Stuff Below
+/////////////////////
+/////////////////////
+/////////////////////
+/////////////////////
+  New Stuff Below */
+
+static struct catg_node *create_catg_node(void)
+{
+	struct catg_node *n = malloc(sizeof(struct catg_node));
+	if (n == NULL) {
+		mem_alloc_fail();
+	}
+	return n;
+}
+
+static struct catg_node *init_head(void)
+{
+	struct catg_node *head = create_catg_node();
+	head->next = NULL;
+	head->prev = NULL;
+	head->data = NULL;
+	return head;
+}
+
+struct catg_node *append_catg_node
+(struct catg_node *head, long catg_fp, struct vec_d *data)
+{
+	struct catg_node *ret = create_catg_node();
+	struct catg_node *tmp = head;
+
+	while (1) {
+		if (tmp->next == NULL) {
+			break;
+		}
+		tmp = tmp->next;
+	}
+
+	tmp->next = ret;
+	ret->next = NULL;
+	ret->prev = tmp;
+	ret->catg_fp = catg_fp;
+	ret->data = data;
+	return ret;
+}
+
+struct catg_node *insert_catg_node
+(struct catg_node **head, size_t idx, long catg_fp, struct vec_d *data)
+{
+	size_t i;
+	struct catg_node *tmp = NULL;
+	struct catg_node *prev = NULL;
+	struct catg_node *ret = create_catg_node();
+
+	tmp = *head;
+
+	if (idx == 0) {
+		ret->next = tmp;
+		ret->prev = NULL;
+		ret->catg_fp = catg_fp;
+		ret->data = data;
+		(*head)->prev = ret;
+		*head = ret;
+		return ret;
+	}
+
+	for (i = 0; i < idx; i++) {
+		if (i == idx - 1) {
+			prev = tmp;
+		}
+		if (tmp == NULL && i != idx) {
+			free(ret);
+			ret = NULL;
+			return NULL;
+		}
+		tmp = tmp->next;
+	}
+
+	if (prev->next->next != NULL) {
+		prev->next->prev = ret;
+	}
+	prev->next = ret;
+	ret->next = tmp;
+	ret->prev = prev;
+	ret->catg_fp = catg_fp;
+	ret->data = data;
+	return ret;
+}
+
+void delete_catg_node(struct catg_node *head, size_t idx)
+{
+	size_t i;
+	struct catg_node *tmp = head;
+	struct catg_node *prev = NULL;
+
+	for (i = 0; i < idx; i++) {
+		if (i == idx - 1) {
+			prev = tmp;
+		}
+		tmp = tmp->next;
+		if (tmp == NULL) {
+			puts("Failed to delete node: node does not exist");
+			exit(1);
+		}
+	}
+
+	if (tmp->next->next != NULL) {
+		prev->next = tmp->next->next;
+		prev->next->prev = prev;
+	} else {
+		prev->next = NULL;
+	}
+
+	free(tmp->data);
+	free(tmp);
+}
+
+void free_catg_nodes(struct catg_node *head)
+{
+	struct catg_node *curr = head;
+	struct catg_node *to_free = NULL;
+	struct vec_d *to_free_vec = NULL;
+	
+	while (1) {
+		if (curr == NULL) {
+			break;
+		}
+		to_free = curr;
+		to_free_vec = curr->data;
+		curr = curr->next;
+		free(to_free);
+		free(to_free_vec);
+	}
+}
+
+void debug_print_catg_node_data(struct catg_node *head)
+{
+	move(0,0);
+	struct catg_node *tmp = head;
+	size_t i = 0;
+
+	while (1) {
+		if (tmp == NULL) {
+			break;
+		}
+		printw("Node: %lu, Addr: %p, Next: %p, Prev: %p, Data: %ld\n", 
+		       i,
+		       (void *)tmp, 
+		       (void *)tmp->next,
+		 	   (void *)tmp->prev, 
+		 	   tmp->catg_fp);
+		i++;
+		tmp = tmp->next;
+	}
+}
+
+struct catg_node *create_catg_node_list(int m, int y) {
+	struct vec_d         *catg_fpis = get_budget_catg_by_date_bo(m, y);
+	struct vec_d         *chunk = get_records_by_mo_yr(m, y);
+	struct catg_node     *head = init_head();
+	struct budget_tokens *budget_tokens = NULL;
+	struct vec_d         *recs = NULL;
+
+	for (size_t i = 0; i < catg_fpis->size; i++) {
+		budget_tokens = tokenize_budget_fpi(catg_fpis->data[i]);
+		recs = get_records_by_any(m, -1, y, 
+							      budget_tokens->catg,
+							      NULL, -1, -1, chunk);
+		free_budget_tokens(budget_tokens);
+		if (i > 0) {
+			append_catg_node(head, catg_fpis->data[i], recs);
+		} else {
+			head->catg_fp = catg_fpis->data[i];
+			head->data = recs;
+		}
+	}
+
+	free(chunk);
+	free(catg_fpis);
+
+	return head;
 }
